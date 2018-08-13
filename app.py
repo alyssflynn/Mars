5686e6050305d526f2fdc9e287af19896b06fdbd
Alyss Flynn <alyss.flynn@me.com>
2018-08-13 15:59:42 -0500

    edited paths

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ def scraper():
         mars_info,
         upsert=True
     )
-    return redirect("http://localhost:5000/", code=302)
+    return redirect("/", code=302)
+    # return redirect("http://localhost:5000/", code=302)
     # return redirect(url_for('index'))
 
 if __name__=="__main__":
